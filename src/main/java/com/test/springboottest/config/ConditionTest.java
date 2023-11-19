a1d5d44e046329883b9901fb57cf0517e4bfc9de
hygge <2662483910@qq.com>
2023-11-19 13:59:30 +0800

    20231119

---
+++ src/main/java/com/test/springboottest/config/ConditionTest.java
@@ -0,0 +1,8 @@
+package com.test.springboottest.config;/**
+*@program: SimpleTest
+*@description: 
+*@author: hugge
+*@create: 2023/09/19
+*/
+public class ConditionTest {
+}
---
+++ src/test/java/com/test/springboottest/myTest/enumTest/ResultCodeTest.java
@@ -0,0 +1,8 @@
+package com.test.springboottest.myTest.enumTest;/**
+*@program: SimpleTest
+*@description: 
+*@author: hugge
+*@create: 2023/09/16
+*/
+public class ResultCodeTest {
+}
